2aa5836e211f9a071a62ca375be70bb9d18daf23
naga6335 <y.nagami9970@gmail.com>
2021-02-13 16:48:23 +0900

    fix: destroy unnecessary files

--- app/controllers/guestsession_controller.rb
+++
@@ -1,8 +0,0 @@
-class GuestsessionController < ApplicationController
-  def create
-    user = User.find_or_create_by!(name: guestuser, email: 'guest@example.com') do |user|
-    user.password = SecureRandom.urlsafe_base64
-    end
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
-end
--- app/helpers/guestsession_helper.rb
+++
@@ -1,2 +0,0 @@
-module GuestsessionHelper
-end
