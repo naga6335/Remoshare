34b772f4b9bed6c789e47a3d75047c74d0c8b53b
naga6335 <y.nagami9970@gmail.com>
2021-02-15 14:37:58 +0900

    feat: add post show edit delete #3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :edit, :destroy]
-  before_action :correct,        only: [:destroy]
+
   def index
     @posts = Post.all.includes(:user).order('created_at DESC')
   end
@@ -19,24 +19,35 @@ class PostsController < ApplicationController
       flash[:success] = "投稿しました！"
       redirect_to root_url
     else
+      flash[:alert] = "入力に誤りがあります"
       render 'new'
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "更新しました！"
+      redirect_to root_url
+    else
+      flash[:alert] = "入力に誤りがあります"
+      render 'edit'
+    end
+  end
+
   def destroy
-    post.destroy
-    flash[:success] = "投稿を削除しました"
-    redirect_to request.referrer || root_url
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_url
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content, :image)
-  end
-
-  def correct
-    post = current_user.posts.find_by(id: params[:id])
-    redirect_to root_url if @post.nil?
   end
 end
