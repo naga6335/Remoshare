c245ba4f5d66b169a990e72401b63aa1691f4620
naga6335 <y.nagami9970@gmail.com>
2021-02-21 18:14:36 +0900

    style: ユーザー一覧ページの作成#20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @users = User.page(params[:page]).per(20)
+    @user = User.find_by(params[:id])
   end
 
   def show
