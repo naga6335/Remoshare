45b3e82431652c223f5ed4a9a75b7f8ec8a3ccec
naga6335 <y.nagami9970@gmail.com>
2021-03-06 17:33:35 +0900

    style: 商品検索ページのスタイリング#59

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,5 +1,5 @@
 RakutenWebService.configure do |c|
-  c.application_id = '1029363210253719272'
+  c.application_id = Rails.application.credentials.rakuten[:api_key]
 
   # (任意) 楽天アフィリエイトID
   c.affiliate_id = '*******************'
