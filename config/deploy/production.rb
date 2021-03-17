6ece883cb23dcc55b8e9daa7cfe7166c64abd6d6
naga6335 <y.nagami9970@gmail.com>
2021-03-18 00:06:38 +0900

    chore: CircleCI/CDの設定#71

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 server "54.168.50.41", user: "nagami", roles: %w{app db web}
 
 set :ssh_options, {
-  keys: %w(~/.ssh/remoshare.pem),
+  keys:[ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
-  auth_methods: %w(publickey),
+  auth_methods: %w[publickey]
 }
 
 # server-based syntax
