39929fe5483125a106d2d6342455f6d5b868e997
naga6335 <y.nagami9970@gmail.com>
2021-03-24 23:59:32 +0900

    refactor: tweetリンクの再設定#71

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validate :user_id
-  validate :post_id
+  validate :user_id, presence: true, uniqueness: { scope: :post_id }
+  validate :post_id, presence: true
 end
