184596393431b3d46f791a9de43302745345d3ee
naga6335 <y.nagami9970@gmail.com>
2021-03-24 18:12:04 +0900

    style: cssの調整#71

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     content { 'a' * 199 }
     issue { 'example' }
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/image.sample.png')) }
-    review { 3 }
     association :user
   end
 end
