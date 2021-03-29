45eeeb7788a7ceede99a2057d8355ab3c1f5e0ed
naga6335 <y.nagami9970@gmail.com>
2021-03-29 16:34:30 +0900

    refactor: 不要ファイル削除、認証周りUX調整#71

--- test/controllers/post_pages_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PostPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get post_pages_home_url
-    assert_response :success
-  end
-end
