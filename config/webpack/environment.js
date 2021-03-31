433f001a29b787d5fe402c5c34d4f8e61f1f204f
naga6335 <y.nagami9970@gmail.com>
2021-03-31 11:24:47 +0900

    fix: javascript文法への置き換え#112

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,2 @@
 const { environment } = require('@rails/webpacker')
-
-const webpack = require('webpack')
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    jquery: 'jquery/src/jquery',
-  })
-)
-
 module.exports = environment
